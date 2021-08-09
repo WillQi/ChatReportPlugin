f6b79f60ecba2fb22024f309766aa96b18f11bf9
WillQi <williamqipizza@gmail.com>
2021-08-09 17:10:26 -0600

    add UsersModel

---
+++ website/src/models/users.js
@@ -0,0 +1,49 @@
+const bcrypt = require('bcrypt');
+const { sqlConnection } = require('../utility/database');
+
+const GET_USER_BY_USERNAME_STMT = 'SELECT id, password, is_admin FROM users WHERE username=?';
+const CREATE_USER_STMT = 'INSERT INTO users (username, password, is_admin) VALUES (?, ?, ?)';
+const DELETE_USER_STMT = 'DELETE FROM users WHERE id=?';
+
+const SALT_ROUNDS = 8;
+
+class UsersModel {
+
+    async getUserByUsername(username) {
+        const [[row]] = await sqlConnection.query(GET_USER_BY_USERNAME_STMT, [username]);
+        if (!row) {
+            return null;
+        }
+        const { id, password: hash, is_admin } = row;
+        
+        return {
+            id,
+            hash,
+            isAdmin: !!is_admin
+        };
+    }
+
+    async createUser(username, rawPassword, isAdmin = false) {
+        // generate a hash for the password
+        const salt = await bcrypt.genSalt(SALT_ROUNDS);
+        const hash = await bcrypt.hash(rawPassword, salt);
+
+        await sqlConnection.execute(CREATE_USER_STMT, [username, hash, isAdmin]);
+    }
+
+    async deleteUser(id) {
+        await sqlConnection.emit(DELETE_USER_STMT, [id]);
+    }
+
+    async checkLogin(username, password) {
+        const user = await this.getUserByUsername(username);
+        if (!user) return false;
+
+        const validLogin = await bcrypt.compare(password, user.hash);
+        return validLogin;
+    }
+
+}
+
+const usersModel = new UsersModel();
+module.exports = usersModel;
