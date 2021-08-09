ab60ec5f0830d22dfa1c9cba281e9fbc2f3af69e
WillQi <williamqipizza@gmail.com>
2021-08-09 15:28:21 -0600

    setup database script

---
+++ website/README.md
@@ -0,0 +1,9 @@
+# ChatReports - Website
+
+This is the website component of ChatReport. It allows staff members to view chat reports on the web through a dashboard and dish out bans/mutes.
+
+## Installation
+- `npm install`
+- Rename `example.env` to `.env` and fill in the details with your site credentials.
+- `npm run-script setup` to setup the database tables
+- `npm run-script start` to start the website
--- website/src/scripts/setup.js
+++ website/src/scripts/setup.js
@@ -1,3 +1,58 @@
 // Setup database tables
 require('dotenv').config();
+const { sqlConnection } = require('../utility/database');
 
+// who can access the site
+const CREATE_USERS_TABLE = `CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    uuid INT NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    password CHAR(60) NOT NULL,
+    is_admin BOOL NOT NULL,
+    PRIMARY KEY(id)
+);`;
+
+// reports made
+const CREATE_REPORTS_TABLE = `CREATE TABLE IF NOT EXISTS reports (
+    id INT NOT NULL AUTO_INCREMENT,
+    reporter_uuid VARCHAR(36) NOT NULL,
+    assigned_to INT,
+    created_at TIMESTAMP NOT NULL,
+    resolved_at TIMESTAMP,
+    FOREIGN KEY(assigned_to) REFERENCES users(id),
+    PRIMARY KEY(id)
+);`;
+
+// chat logs
+const CREATE_LOG_TABLE = `CREATE TABLE IF NOT EXISTS chat_logs (
+    id INT NOT NULL AUTO_INCREMENT,
+    report_id INT NOT NULL,
+    uuid VARCHAR(36) NOT NULL,
+    username VARCHAR(16) NOT NULL,
+    message TEXT(32767) NOT NULL,
+    FOREIGN KEY(report_id) REFERENCES reports(id),
+    PRIMARY KEY(id)
+);`;
+
+const CREATE_PUNISHMENTS_TABLE = `CREATE TABLE IF NOT EXISTS punishments (
+    id INT NOT NULL AUTO_INCREMENT,
+    report_id INT NOT NULL,
+    type TINYINT NOT NULL,
+    assigned_at TIMESTAMP NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    FOREIGN KEY(report_id) REFERENCES reports(id),
+    PRIMARY KEY(id) 
+);`;
+
+(async function() {
+    try {
+        await sqlConnection.execute(CREATE_USERS_TABLE);
+        await sqlConnection.execute(CREATE_REPORTS_TABLE);
+        await sqlConnection.execute(CREATE_LOG_TABLE);
+        await sqlConnection.execute(CREATE_PUNISHMENTS_TABLE);
+        console.log('Successfully setup database tables!');
+        process.exit();
+    } catch (error) {
+        console.error(error);
+    }
+})();
