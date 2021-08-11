5d7fdf3c1ee2a4e414f9b6c038c54a19445c1c9a
WillQi <williamqipizza@gmail.com>
2021-08-10 22:28:57 -0600

    basic page to view chat log

--- website/src/controllers/dashboard.js
+++ website/src/controllers/dashboard.js
@@ -5,6 +5,7 @@ const { loggedIn } = require('../middleware/session');
 const reportModel = require('../models/reports');
 
 const dashboardView = require('../views/dashboard/dashboard.marko').default;
+const reportView = require('../views/dashboard/report.marko').default;
 
 const router = new Router();
 
@@ -15,8 +16,18 @@ router.get('/dashboard', loggedIn, async function (request, response) {
     });
 });
 
-router.get('/dashboard/reports/:id', loggedIn, csrf, function (request, response) {
-    // TODO: fetch report and display
+router.get('/dashboard/reports/:id', loggedIn, csrf, async function (request, response) {
+    const id = parseInt(request.params.id);
+    if (isNaN(id) || !isFinite(id)) {
+        response.redirect('/dashboard');
+        return;
+    }
+
+    const chatLog = await reportModel.getReportChatLog(id);
+    response.marko(reportView, {
+        chatLog,
+        csrfToken: request.csrfToken()
+    });
 });
 
 router.post('/dashboard/reports/:id', loggedIn, csrf, function (request, response) {
